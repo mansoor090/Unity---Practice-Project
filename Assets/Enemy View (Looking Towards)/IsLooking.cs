7b42565d586279e45392c28ec4c5b2c755f712f8
mansoor090 <mansoor.ah187@gmail.com>
2021-05-15 18:37:13 +0500

    Uploaded  ( Is something is in front ) Usage: To give vision to our enemy to check if player is in front.

---
+++ Assets/Enemy View (Looking Towards)/IsLooking.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IsLooking : MonoBehaviour
+{
+
+
+    public Transform Player;
+    public float     dotVlaue  = 0;
+    public bool      isLooking = false;
+    void OnDrawGizmos()
+    {
+        Vector3 pt = Player.position;
+        pt = pt.normalized;
+
+        dotVlaue = Vector3.Dot(transform.forward, (Player.position   - transform.position).normalized);
+        
+        if (dotVlaue > 0f)
+        {
+            isLooking = true;
+        }
+        else
+        {
+            isLooking = false;
+        }
+        Gizmos.DrawLine(transform.position, pt);
+        
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
